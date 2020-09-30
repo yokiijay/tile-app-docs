9da0515c5808852836c847d50cbb3f598b1d7976
Yokiijay <yokiijay@gmail.com>
2020-09-30 22:14:55 +0800

    Update download.tsx

--- pages/download.tsx
+++ pages/download.tsx
@@ -7,6 +7,7 @@ import Navigation from '../components/Navigation'
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
 import { theme } from '../config/theme'
+import { FaApple, FaWindows } from 'react-icons/fa'
 
 const DownloadPage: FC = () => {
   const centerObjectTitleCSS = css`
@@ -29,18 +30,49 @@ const DownloadPage: FC = () => {
       <Head>
         <title>Tile App - Download</title>
       </Head>
-
       
       <motion.div layoutId='title-text' transition={{type: 'spring', stiffness: 300, damping: 30}} style={{
-          position: 'absolute',
-          top: 30,
-          left: 20
-        }}>
-          <Box className='center-object-title' css={centerObjectTitleCSS} borderBox>
-            <h1>Tile App.</h1>
-            <p>Download the Tile App. MacOs, Win supported.</p>
+        position: 'absolute',
+        top: 30,
+        left: 20
+      }}>
+        <Box className='center-object-title' css={centerObjectTitleCSS} borderBox>
+          <h1>Tile App.</h1>
+          <p>Download the Tile App. MacOs, Win supported.</p>
+        </Box>
+      </motion.div>
+
+      <Box className='download' row center css={css`
+        h1 {
+          max-width: 320px;
+          margin: 0;
+        }
+        .download {
+
+          &__item {
+            background: ${theme.black};
+            color: white;
+            padding: 12px 20px;
+            width: 100%;
+          }
+        }
+      `}>
+        <Box center left>
+          <h1 className='download__words'>Download</h1>
+          <p>Choose your platform.</p>
+        </Box>
+
+        <Box className='download__divider' height='100%' width='1px' mx={4} bg={theme.dark[6]}></Box>
+
+        <Box>
+          <Box className="download__item" row mb={3} pointer>
+            <FaApple /> <Box ml={3}><a href="https://github.com/yokiijay/tile-app-docs/releases/download/0.1.0/Tile.App-0.1.0.dmg" download="Tile App">For Mac</a></Box>
           </Box>
-        </motion.div>
+          <Box className="download__item" row pointer>
+            <FaWindows /> <Box ml={3}><a href="https://github.com/yokiijay/tile-app-docs/releases/download/0.1.0/Tile.App.Setup.0.1.0.exe" download="Tile App">For Windows</a></Box>
+          </Box>
+        </Box>
+      </Box>
 
       <Navigation currentPath='download' />
     </Layout>
